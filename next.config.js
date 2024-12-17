f8532623eaa9a94011b3ff5cb1c5318b2fc8e244
NikhilPathaniaa <developernikhil1@gmail.com>
2024-12-17 18:35:35 +0530

    latest update

--- next.config.js
+++ next.config.js
@@ -1,38 +1,15 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-const { URL } = require('url');
-
 const nextConfig = {
   output: 'export',
-  distDir: 'dist',
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: false
-  },
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
   experimental: {
     serverActions: {
-      bodySizeLimit: '2mb',
       allowedOrigins: ['localhost:3000', 'chitchat-blog.vercel.app'],
     },
   },
-  generateBuildId: async () => {
-    return 'build-' + Date.now()
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        tls: false,
-        dns: false,
-      };
-    }
-    return config;
-  }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
